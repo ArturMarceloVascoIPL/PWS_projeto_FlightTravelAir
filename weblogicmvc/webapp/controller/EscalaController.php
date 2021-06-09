c1a0e0401816bfe26b61a65360adc71c8c16ec49
VascoSilva <vasco.silva99@sapo.pt>
2021-06-09 23:28:19 +0100

    Melhoramento Escala
    - Tem bugs mas já cria e apaga escalas

--- weblogicmvc/webapp/controller/EscalaController.php
+++ weblogicmvc/webapp/controller/EscalaController.php
@@ -30,35 +30,39 @@ class EscalaController extends BaseAuthController
         return View::make('escala.create', ['aeroportos' => $aeroportos, 'idvoo' => $idvoo]);
     }
 
-    public function store()
+    public function store($idvoo)
     {
         $this->loginFilterByRole('gestorvoo');
 
         //create new resource (activerecord/model) instance with data from POST
         //your form name fields must match the ones of the table fields
         $escala = new Escala(Post::getAll());
+
         if ($escala->is_valid()) {
             $escala->save();
-            Redirect::toRoute('escala/index');
+
+            $escalas = Escala::all(array('conditions' => array('idvoo = ?', $idvoo)));
+            Redirect::toRoute('escala/index', ['idvoo' => $idvoo]);
         } else {
             //redirect to form with data and errors
-            Redirect::flashToRoute('escala/create', ['escala' => $escala]);
+            Redirect::flashToRoute('escala/create', ['escala' => $escala, 'idvoo' => $idvoo]);
         }
     }
 
-    public function edit($idescala)
+    public function edit($idescala, $idvoo)
     {
         $this->loginFilterByRole('gestorvoo');
 
+        $aeroportos = Aeroporto::all();
         $escala = Escala::find([$idescala]);
         if (is_null($escala)) {
             //TODO redirect to standard error page
         } else {
-            return View::make('escala.edit', ['escala' => $escala]);
+            return View::make('escala.edit', ['aeroportos' => $aeroportos, 'escala' => $escala, 'idvoo' => $idvoo]);
         }
     }
 
-    public function update($idescala)
+    public function update($idescala, $idvoo)
     {
         $this->loginFilterByRole('gestorvoo');
 
@@ -69,19 +73,19 @@ class EscalaController extends BaseAuthController
 
         if ($escala->is_valid()) {
             $escala->save();
-            Redirect::toRoute('escala/index');
+            Redirect::toRoute('escala/index', ['idvoo' => $idvoo]);
         } else {
             //redirect to form with data and errors
             Redirect::flashToRoute('escala/edit', ['escala' => $escala]);
         }
     }
 
-    public function destroy($idescala)
+    public function destroy($idescala, $idvoo)
     {
         $this->loginFilterByRole('gestorvoo');
 
         $escala = Escala::find([$idescala]);
         $escala->delete();
-        Redirect::toRoute('escala/index');
+        Redirect::toRoute('escala/index', ['idvoo' => $idvoo]);
     }
 }
